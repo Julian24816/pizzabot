c397d9e9c2d1d7f3568184f88f3aaf93fc9e64dd
Paul Schwind <git@pschwind.de>
2022-12-29 17:03:26 +0100

    order list

--- frontend/src/components/OrderSummary.svelte
+++ frontend/src/components/OrderSummary.svelte
@@ -1,6 +1,45 @@
 <script>
     import {link} from 'svelte-spa-router';
+
+    async function fetchOrders() {
+        const response = await fetch('/order');
+        return await response.json();
+    }
 </script>
 
 
 <a href="/" use:link>Create</a>
+
+
+{#await fetchOrders() then orders}
+    <table>
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Meat</th>
+            <th>Vegetarian</th>
+            <th>Vegan</th>
+        </tr>
+        </thead>
+        <tbody>
+        {#each orders as order}
+            <tr>
+                <td>
+                    { order.user_name }:
+                </td>
+                <td>
+                    { order.number_of_meat_pieces }
+                </td>
+                <td>
+                    { order.number_of_vegetarian_pieces }
+                </td>
+                <td>
+                    { order.number_of_vegan_pieces }
+                </td>
+            </tr>
+        {/each}
+        </tbody>
+    </table>
+{:catch error}
+    <p>Failed to load orders</p>
+{/await}
