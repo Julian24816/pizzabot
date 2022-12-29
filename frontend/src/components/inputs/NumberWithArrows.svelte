a210988068e5f2f4225f206a5f167a1b5f8e59bb
Paul Schwind <git@pschwind.de>
2022-12-29 20:13:59 +0100

    some nice UI improvements, also for mobile
    Signed-off-by: Paul Schwind <git@pschwind.de>

---
+++ frontend/src/components/inputs/NumberWithArrows.svelte
@@ -0,0 +1,25 @@
+<script>
+
+    export let value;
+    export let placeholder = undefined;
+    export let min = undefined;
+    export let max = undefined;
+
+    function up() {
+        if (max === undefined || value < max) {
+            value++;
+        }
+    }
+
+    function down() {
+        if (min === undefined || value > min) {
+            value--;
+        }
+    }
+
+</script>
+
+
+<button on:click={ up }>⬆️</button>
+<input { max } { min } { placeholder } bind:value type="number"/>
+<button on:click={ down }>⬇️</button>
